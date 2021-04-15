45782bd1b4414e21b4a9a0722bb67dc1cdf3f29e
Timur Çelikel <me@timurcelikel.com>
2021-04-15 18:30:53 +0200

    add contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,17 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+});
 
 module.exports = {
   /* Your site config here */
-  plugins: ["gatsby-plugin-dts-css-modules"],
+  plugins: [
+    "gatsby-plugin-dts-css-modules",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: "u0ztoekrenxw",
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 };
