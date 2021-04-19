e47e78fea4d64086dc578bd7bae4f8455528d5ba
Timur Çelikel <me@timurcelikel.com>
2021-04-19 21:34:36 +0200

    add font

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import * as s from "./style.module.css";
+import "./style.css";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return <main className={s.main}>{children}</main>;
