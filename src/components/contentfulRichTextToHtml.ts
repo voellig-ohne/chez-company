24d759e7a62d580580f790e064b22012887465ad
Timur Çelikel <me@timurcelikel.com>
2021-04-29 15:11:04 +0200

    text: add line breaks, bump up line height

--- src/components/contentfulRichTextToHtml.ts
+++
@@ -1,8 +0,0 @@
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-
-export default function contentfulRichtTextToThml(source?: string) {
-    if (!source) return null;
-    const parsed = JSON.parse(source);
-
-    return documentToReactComponents(parsed);
-}
---
+++ src/components/contentfulRichTextToHtml.tsx
@@ -0,0 +1,14 @@
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import React from 'react';
+
+export default function contentfulRichtTextToThml(source?: string) {
+    if (!source) return null;
+    const parsed = JSON.parse(source);
+
+    const options = {
+        renderText: (text: string) =>
+            text.split('\n').flatMap((text, i) => [i > 0 && <br />, text]),
+    };
+
+    return documentToReactComponents(parsed, options);
+}
