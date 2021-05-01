0a00dcae186d41a5690baa272743fe7214a52af6
Timur Çelikel <me@timurcelikel.com>
2021-05-01 12:18:15 +0200

    add flags that are supposed to speed up things

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ require('dotenv').config({
 });
 
 module.exports = {
+    flags: { PRESERVE_WEBPACK_CACHE: true, FAST_DEV: true },
     plugins: [
         `gatsby-plugin-image`,
         `gatsby-plugin-sharp`,
