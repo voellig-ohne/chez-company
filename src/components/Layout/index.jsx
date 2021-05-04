6debea16cf4e75074d4c0aab6c7223984456d1c2
Timur Çelikel <me@timurcelikel.com>
2021-05-04 16:07:10 +0200

    Layout: add default description and OG image

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -5,19 +5,40 @@ import { ProjectGraph } from '../ProjectGraph';
 import Navigation from '../Navigation';
 import { Helmet } from 'react-helmet';
 import Favicon from './favicon.png';
+import { graphql, useStaticQuery } from 'gatsby';
 
 export default function Layout({ children, path }) {
+    const {
+        contentfulGlobal: { title, description, ogimage },
+    } = useStaticQuery(graphql`
+        query GlobalStuffQuery {
+            contentfulGlobal(
+                id: { eq: "7a2518b6-52e7-59d7-b0a0-1a812f2980b9" }
+            ) {
+                title
+                description {
+                    description
+                }
+                ogimage {
+                    resize(width: 1000) {
+                        src
+                    }
+                }
+            }
+        }
+    `);
+
     return (
         <>
-            <Helmet
-                defaultTitle="Chez Company"
-                titleTemplate="Chez Company - %s"
-            >
+            <Helmet defaultTitle={title} titleTemplate={`${title} - %s`}>
                 <link rel="icon" type="image/png" href={Favicon} />
                 <link
                     rel="canonical"
                     href={`https://chez-company.org${path}`}
                 />
+                <meta name="description" content={description?.description} />
+                <meta property="og:title" content={title} />
+                <meta property="og:image" content={ogimage?.resize?.src} />
             </Helmet>
             <div className={s.graph}>
                 <ProjectGraph />
