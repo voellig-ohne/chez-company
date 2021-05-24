5e85b898bfb6efc21c569865d49a23a0f9061b17
Timur Çelikel <me@timurcelikel.com>
2021-05-24 18:01:43 +0200

    useProjects: hide dummy project

--- src/hooks/useProjects.ts
+++ src/hooks/useProjects.ts
@@ -81,6 +81,7 @@ export const useProjects = () => {
                                     }
                                 }
                             }
+                            hideInGraph
                         }
                     }
                 }
