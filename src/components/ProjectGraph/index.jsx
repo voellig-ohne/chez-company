5f9e3afe4011951c7b557618ca125b2e88a43c88
Timur Çelikel <me@timurcelikel.com>
2021-05-26 18:13:24 +0200

    ProjectGraph: zoom in on all pages

--- src/components/ProjectGraph/index.jsx
+++ src/components/ProjectGraph/index.jsx
@@ -64,7 +64,7 @@ export function ProjectGraph({ projectFocusId, path }) {
             node => node.slug && getRoute(node) === path
         )?.id;
 
-        if (path === '/' && !hasCenteredOnce) {
+        if (!nodeIdToFocus && !hasCenteredOnce) {
             nodeIdToFocus = projectFocusId;
         }
 
